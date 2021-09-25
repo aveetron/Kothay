5135447fc73d1d2246f5c119740856fa92ed3eaa
aveetron <aveechakra@gmail.com>
2021-09-25 10:19:01 +0600

    company operation models added

--- company_operation/models.py
+++ company_operation/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    status = models.BooleanField(default=True)
+
+class OperationStatus(BaseModel):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Service(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    details = models.TextField(null=True, blank=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=3, null=True, blank=True)
+
+    def __str__(self):
+        return self.name + '-' + self.cost
+
+
+class Designation(BaseModel):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
